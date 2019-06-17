02fdcf9d5cbcc3c344e21c15597771d923e51085
Matheus Rossetti <matheus__rossetti@hotmail.com>
2019-06-17 17:23:32 -0300

    170620191723

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+    Para compilar no terminal use:
+    gcc main.c -o -lssl -lcrypto main
+*/
+
+#include "inode.h"
+#include "sha256.c"
+#include <openssl/evp.h>
+#include <openssl/ssl.h>
+#include <openssl/rsa.h>
+#include <openssl/x509.h>
+
+
+//Declaração função
+
+int main()
+{
+
+	return 0;	
+}
