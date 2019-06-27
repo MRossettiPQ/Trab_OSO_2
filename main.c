35b272324ba0f75ea35f15dfee0a029c07ce22d7
Matheus Rossetti <matheus__rossetti@hotmail.com>
2019-06-27 18:39:44 -0300

    270620191839

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
     fs-case6.bin        dados   - hash: a54859682dcd9d4c15d0927da68c502b62ff8cc9032e6acdb7de4398d2399e84
 */
 //Importa arquivos pasta
-#include "biblioteca.h"                                                  // 
+#include "biblioteca.h"                                                         //Inclui algumas bibliotecas: unisted, openssl, sha256, inode.
 //Declaração função
 FILE*   criaSistemaArquivos     (char argc, char ** argv);
 void    verificaArquivo         (char argc, char ** argv);
@@ -28,55 +28,41 @@ int main(char argc, char ** argv)
     char cmdCC[90], auxSprintf, *questiona = (char*)malloc(sizeof(char));
     printf("\nResposta: %s", questiona);
 
-
-        printf("\n Decide a função");
-        if(0 == strcmp(argv[1], "init"))
-        {
-            // CRIA SISTEMA DE ARQUIVO      - init "nome arquivo.bin" "bytes do bloco" "Qt. de Blocos" "Qt. Inodes"
-            printf("\n Cria Sistema de Arquivos");
-            arqDisco = criaSistemaArquivos(argc, argv);
-            printf("\n Sistema de Arquivos Criado\n");
-        }
-        else if(0 == strcmp(argv[1], "add"))
-        {
-            // ADICIONA ARQUIVO             - add "nome arquivo.bin" "bytes do bloco" "Qt. de Blocos" "Qt. Inodes"
-            /* code */
-        }
-        else if(0 == strcmp(argv[1], "addDir"))
-        {
-            // ADICIONA DIRETORIO           - addDir "nome arquivo.bin" "bytes do bloco" "Qt. de Blocos" "Qt. Inodes"
-            /* code */
-        }    
-        else if(0 == strcmp(argv[1], "sha256"))
-        {
-            // IMPRIME A HASH               - sha256 "nome arquivo.bin" "bytes do bloco" "Qt. de Blocos" "Qt. Inodes"
-            char pathSis[256];
-            getcwd(pathSis, sizeof(pathSis));
-            printf("\n Imprime a HASH");
-            verificaArquivo(argc, argv);                                        //Verifica se o .bin passado existe
-            auxSprintf = sprintf(cmdCC, "%s/%s", pathSis, argv[2]);             //Gera o path para o arquivo desejado
-            printf("\n\nPath: %s\n\nHash: ", cmdCC);                            //Imprime o PATH do diretorio do programa
-            printSha256(cmdCC);                                                 //Imprime a HASH
-        }
-        else if(0 == strcmp(argv[1], "debug"))
-        {
-            // IMPRIME DADOS DO Bin         - debug "nome arquivo.bin"
-            printf("\n Imprime os metadados do 'DISCO'");
-            debugArquivo(argc, argv);
-        }
-
-    /*do
+    printf("\n Decide a função");
+    if(0 == strcmp(argv[1], "init"))
     {
-        //Caso queira fazer programa continuo
-        printf("\nDeseja executar outro comando?\n");
-			scanf("%s", questiona);
-			scanf(" %s", argv[1]); //Função
-			scanf(" %s", argv[2]); //Nome do arquivo
-			scanf(" %s", argv[3]); //
-            scanf(" %s", argv[4]); //
-            scanf(" %s", argv[5]); //Nr. Inode
-    } while((0 == strcmp(questiona, "s")) || (0 == strcmp(questiona, "S")) || (0 == strcmp(questiona, "SIM")) || (0 == strcmp(questiona, "sI")) || (0 == strcmp(questiona, "SI")) || (0 == strcmp(questiona, "Sim")));
-    */
+        // CRIA SISTEMA DE ARQUIVO      - init "nome arquivo.bin" "bytes do bloco" "Qt. de Blocos" "Qt. Inodes"
+        printf("\n Cria Sistema de Arquivos");
+        arqDisco = criaSistemaArquivos(argc, argv);
+        printf("\n Sistema de Arquivos Criado\n");
+    }
+    else if(0 == strcmp(argv[1], "add"))
+    {
+        // ADICIONA ARQUIVO             - add "nome arquivo.bin" "bytes do bloco" "Qt. de Blocos" "Qt. Inodes"
+        /* code */
+    }
+    else if(0 == strcmp(argv[1], "addDir"))
+    {
+        // ADICIONA DIRETORIO           - addDir "nome arquivo.bin" "bytes do bloco" "Qt. de Blocos" "Qt. Inodes"
+        /* code */
+    }    
+    else if(0 == strcmp(argv[1], "sha256"))
+    {
+        // IMPRIME A HASH               - sha256 "nome arquivo.bin" "bytes do bloco" "Qt. de Blocos" "Qt. Inodes"
+        char pathSis[256];
+        getcwd(pathSis, sizeof(pathSis));
+        printf("\n Imprime a HASH");
+        verificaArquivo(argc, argv);                                        //Verifica se o .bin passado existe
+        auxSprintf = sprintf(cmdCC, "%s/%s", pathSis, argv[2]);             //Gera o path para o arquivo desejado
+        printf("\n\nPath: %s\n\nHash: ", cmdCC);                            //Imprime o PATH do diretorio do programa
+        printSha256(cmdCC);                                                 //Imprime a HASH
+    }
+    else if(0 == strcmp(argv[1], "debug"))
+    {
+        // IMPRIME DADOS DO Bin         - debug "nome arquivo.bin"
+        printf("\n Imprime os metadados do 'DISCO'");
+        debugArquivo(argc, argv);
+    }
 
     printf("\n=============FIM DO PROGRAMA=============\n\n");
 
