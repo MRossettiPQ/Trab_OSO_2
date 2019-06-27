499a92a3bc518f58552b695d1a9a059a1e0b4510
Matheus Rossetti <matheus__rossetti@hotmail.com>
2019-06-27 07:26:44 -0300

    270620190726

---
+++ biblioteca.h
@@ -0,0 +1,20 @@
+#ifndef biblioteca_h
+#define biblioteca_h
+
+    //
+    #include <stdio.h>
+    #include <string.h>                                                 //strlen
+    #include <stdlib.h>                                                 //strlen
+    //
+    #include <sys/types.h>
+    #include <sys/socket.h>
+    //Importa bibliote SSL
+    #include <openssl/evp.h>                                            // 
+    #include <openssl/ssl.h>                                            // 
+    #include <openssl/rsa.h>                                            // 
+    #include <openssl/x509.h>                                           //
+    //Importa arquivos pasta 
+    #include "inode.h"                                                  // 
+    #include "sha256.h"                                                 // 
+
+#endif /* biblioteca_h */
--- main.c
+++ main.c
@@ -1,45 +1,84 @@
 /*
     Para compilar no terminal use:
-    gcc main.c -o -lssl -lcrypto main
+    gcc main.c -o main -lssl -lcrypto 
     para executar ./main
 */
-#include <stdio.h>
 //Importa arquivos pasta
-#include "inode.h"                                               // 
-#include "sha256.c"                                              // 
-//Importa bibliote SSL
-#include <openssl/evp.h>                                         // 
-#include <openssl/ssl.h>                                         // 
-#include <openssl/rsa.h>                                         // 
-#include <openssl/x509.h>                                        // 
+#include "biblioteca.h"                                                  // 
 //Declaração função
+FILE* criaSistemaArquivos(char argc, char ** argv);
+void verificaArquivo(char argc, char ** argv);
 
 
 //Função principal
-int main()
+int main(char argc, char ** argv)
 {
-    INODE Teste;
-
+    FILE* arqDisco;
+    char *cmdCC;
     
-    Teste.DIRECT_BLOCKS;
-    Teste.DOUBLE_INDIRECT_BLOCKS;
-    Teste.INDIRECT_BLOCKS;
-    Teste.IS_DIR;
-    Teste.IS_USED;
-    Teste.NAME;
-    Teste.SIZE;
-    printf("TESTES");
-    printf("DIRECT_BLOCKS: %hhn", Teste.DIRECT_BLOCKS);
-    printf("DOUBLE_INDIRECT_BLOCKS: %hhn", Teste.DOUBLE_INDIRECT_BLOCKS);
-    printf("INDIRECT_BLOCKS: %hhn", Teste.INDIRECT_BLOCKS);
-    printf("IS_DIR: %d", Teste.IS_DIR);
-    printf("IS_USED: %d", Teste.IS_USED);
-    printf("NAME: %s", Teste.NAME);
-    printf("SIZE: %d", Teste.SIZE);
 
+    if(0 == strcmp(argv[1], "init"))
+    {
+        // CRIA SISTEMA DE ARQUIVO  - init "nome arquivo.bin" "bytes do bloco" "Qt. de Blocos" "Qt. Inodes"
+        arqDisco = criaSistemaArquivos(argc, argv);
+    }
+    else if(0 == strcmp(argv[1], "add"))
+    {
+        /* code */
+    }
+    else if(0 == strcmp(argv[1], "addDir"))
+    {
+        /* code */
+    }    
+    else if(0 == strcmp(argv[1], "sha256"))
+    {
+        /* code */
+    }
+    else if(0 == strcmp(argv[1], "debug"))
+    {
+        /* code */
+    }
 
-    //FILE *pathArquivo;
-    //pathArquivo = "/";
-    //printSha256(pathArquivo);
+    printf("\n=============FIM DO PROGRAMA=============\n\n");
+
 	return 0;	
+}
+//Implementa função
+void verificaArquivo(char argc, char ** argv)
+{
+    char *cmdCC, n;
+    
+    FILE *fp;
+    fp = fopen(argv[2], "r");
+    //VERIFICA ARQUIVO .bin NO DIRETORIO
+    if (fp!=NULL)
+    { 
+        printf("\n Arquivo no diretorio");
+    }
+    else
+    {
+        printf("\n Arquivo inexistente");
+        n = sprintf(cmdCC, "touch %s", argv[2]);
+        system(cmdCC);
+        printf("\n Arquivo gerado");
+    }
+}
+
+FILE* criaSistemaArquivos(char argc, char ** argv)
+{
+    verificaArquivo(argc, argv);
+    FILE *arquivo;
+
+    arquivo = fopen(argv[2],"r");
+    if (arquivo!=NULL)
+    {
+        //fwrite(&meta, sizeof(struct metadata), 1, arquivo);
+    }
+    else
+    {
+        printf("\nArquivo não foi encontrado \n");
+    }
+    
+
+    return arquivo;
 }
